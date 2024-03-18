4d3aef73f9b721411e813d208b316dd332f130b6
plotiwitolp <plotiwitolp@gmail.com>
2024-03-18 19:14:29 +0400

    init

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function () {
+    // START document ready
+    // -- код --
+    // END document ready
+  });
+})(jQuery);
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- , user-scalable=0 -->
+  <title>Project</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+  <header>
+
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+  <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
